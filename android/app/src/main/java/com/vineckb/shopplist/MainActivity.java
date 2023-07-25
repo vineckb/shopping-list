73888d382d8134fd253fb395e3722eb7a6894189
Vinicius Borges <vineckb@gmail.com>
2023-07-24 21:58:30 -0300

    chore: rename app

--- android/app/src/main/java/com/vineckb/shopplist/MainActivity.java
+++ android/app/src/main/java/com/vineckb/shopplist/MainActivity.java
@@ -1,4 +1,4 @@
-package com.vineckb.shopplist;
+package com.vineckb.shoppinglist;
 
 import android.os.Build;
 import android.os.Bundle;
@@ -13,7 +13,7 @@ import expo.modules.ReactActivityDelegateWrapper;
 public class MainActivity extends ReactActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
-    // Set the theme to AppTheme BEFORE onCreate to support 
+    // Set the theme to AppTheme BEFORE onCreate to support
     // coloring the background, status bar, and navigation bar.
     // This is required for expo-splash-screen.
     setTheme(R.style.AppTheme);
--- android/app/src/main/java/com/vineckb/shopplist/MainApplication.java
+++ android/app/src/main/java/com/vineckb/shopplist/MainApplication.java
@@ -1,4 +1,4 @@
-package com.vineckb.shopplist;
+package com.vineckb.shoppinglist;
 
 import android.app.Application;
 import android.content.res.Configuration;
