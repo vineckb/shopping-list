1ee875921281891cfbe5ebaa726f32791f7a922f
Vinicius Borges <vineckb@gmail.com>
2023-07-25 23:58:47 -0300

    feat: setup expo router

--- src/database/schemas/index.ts
+++ src/database/schemas/index.ts
@@ -1,9 +1,8 @@
 import { appSchema } from '@nozbe/watermelondb';
 import { listSchema } from './list';
-import { listProductSchema } from './listProduct';
 import { productSchema } from './product';
 
 export const schema = appSchema({
   version: 2,
-  tables: [listSchema, productSchema, listProductSchema],
+  tables: [listSchema, productSchema],
 });
