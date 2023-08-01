60989ac95b35b5fef77c646e97bcfd343d59ba6d
Vinicius Borges <vineckb@gmail.com>
2023-08-01 18:15:14 -0300

    up

---
+++ src/app/_layout.tsx
@@ -0,0 +1,20 @@
+import { Stack } from 'expo-router';
+import { PaperProvider } from 'react-native-paper';
+
+export default function Layout() {
+  return (
+    <PaperProvider>
+      <Stack
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#f4511e',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}
+      />
+    </PaperProvider>
+  );
+}
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,24 @@
+import { Link, Stack } from 'expo-router';
 import { Text, View } from 'react-native';
+import { FlatList } from 'react-native';
+import { database } from '../database';
+import { List } from '../database/models/list';
+import { useQuery } from '@tanstack/react-query';
 
 export default function Page() {
+
+  const {data, isLoading} = useQuery({
+    queryFn: () => database.get<List>('lists').query()
+  });
+
   return (
-    <View>
-      <Text>hello world!</Text>
+    <View style={{ flex: 1 }}>
+      <Stack.Screen
+        options={{
+          title: 'Listas de Compras',
+        }}
+      />
+      {isLoading && <FlatList data={data} />}
     </View>
   );
 }
